a6600d0952e7f26b9c33ece5f107b2de4f17d756
Chris Donnelly <chris.donnelly@coxautoinc.com>
2023-02-15 17:26:29 -0600

    feat: client API

---
+++ src/client/index.ts
@@ -0,0 +1,7 @@
+
+export * from './client';
+export * from './no-op-provider';
+//export { OpenFeature } from './open-feature';
+export * from './open-feature';
+export * from './types';
+export * from '../shared';
